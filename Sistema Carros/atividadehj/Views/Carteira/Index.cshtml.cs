ac7af3031ea430571409cab861236e4f7cc3a89b
LuizMiguel78 <149170949+LuizMiguel78@users.noreply.github.com>
2026-05-08 10:04:12 -0300

    Funcionario e Carteira

---
+++ Sistema Carros/atividadehj/Views/Carteira/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace atividadehj.Views.Carteira
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
