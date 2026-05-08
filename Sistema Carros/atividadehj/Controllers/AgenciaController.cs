f4a7c9df05bc49b866dbd56c0b62ed5b184f9312
LuizMiguel78 <149170949+LuizMiguel78@users.noreply.github.com>
2026-05-08 09:26:10 -0300

    Adicionado Sistema Carros

---
+++ Sistema Carros/atividadehj/Controllers/AgenciaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace atividadehj.Controllers
+{
+    public class AgenciaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
